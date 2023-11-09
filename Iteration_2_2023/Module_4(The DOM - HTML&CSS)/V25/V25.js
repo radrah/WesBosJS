43af41c2f405adfed906c98ec7a1ab07b6d2cd34
radrah <87847961+radrah@users.noreply.github.com>
2023-11-10 00:00:59 +1300

    V25 completed, I am sleepy after melatonin gummies

--- Iteration_2_2023/Module_4(The DOM - HTML&CSS)/V25/V25.js
+++ Iteration_2_2023/Module_4(The DOM - HTML&CSS)/V25/V25.js
@@ -1,3 +1,47 @@
 // Creating HTML
 
 console.log('V25');
+
+const myParagraph = document.createElement('p');
+myParagraph.innerText = 'Hello I am P';
+myParagraph.classList.add('h6');
+myParagraph.classList.add('success');
+
+const myImage = document.createElement('img');
+myImage.src = 'https://source.unsplash.com/random/100x100';
+myImage.alt = 'random';
+
+const myDiv = document.createElement('div');
+myDiv.classList.add('wrapper');
+
+
+console.log(myParagraph, myImage, myDiv);
+
+document.body.appendChild(myDiv);
+myDiv.append(myParagraph, myImage);
+
+const heading = document.createElement('h2');
+heading.textContent = 'Cool Things';
+
+myDiv.insertAdjacentElement('beforebegin', heading);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'one';
+const li2 = document.createElement('li');
+li2.innerText = '2';
+const li3 = document.createElement('li');
+li3.innerText = '3';
+const li4 = document.createElement('li');
+li4.innerText = '4';
+const li5 = document.createElement('li');
+li5.textContent = 'five';
+
+ul.appendChild(li2);
+li2.insertAdjacentElement('beforebegin', li1);
+li2.insertAdjacentElement('afterend', li3);
+ul.appendChild(li5); 
+li5.insertAdjacentElement('beforebegin', li4);
+li2.insertAdjacentElement('afterend', li3);
+
+console.log(ul);
