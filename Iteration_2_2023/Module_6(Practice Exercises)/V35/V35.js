180930b6d0e9979183d4cc45777ad0936c19e0f2
radrah <raduwan.rahman@hotmail.com>
2023-11-18 23:18:43 +1300

    just for the commit

---
+++ Iteration_2_2023/Module_6(Practice Exercises)/V35/V35.js
@@ -0,0 +1,10 @@
+// Scroll Events and Intersection Observer
+
+console.log('V35');
+
+const terms = document.querySelector('.terms-and-conditions');
+
+terms.addEventListener('scroll', function(e) {
+  console.log(e);
+});
+// Just want a greeen thing on my github lol
