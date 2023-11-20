cab2825e8e9a0a6636d62a7dd61f997355df7f43
radrah <raduwan.rahman@hotmail.com>
2023-11-21 12:48:14 +1300

    V35 done

--- Iteration_2_2023/Module_6(Practice Exercises)/V35/V35.js
+++ Iteration_2_2023/Module_6(Practice Exercises)/V35/V35.js
@@ -3,8 +3,18 @@
 console.log('V35');
 
 const terms = document.querySelector('.terms-and-conditions');
+const acceptButton = document.querySelector('.accept');
+const watch = document.querySelector('.watch');
 
-terms.addEventListener('scroll', function(e) {
-  console.log(e);
-});
-// Just want a greeen thing on my github lol
+function obCallback(payload) {
+  if(payload[0].intersectionRatio === 1) {
+    acceptButton.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {root: terms, threshold: 1});
+
+// console.log(terms.lastElementChild);
+ob.observe(terms.lastElementChild);
